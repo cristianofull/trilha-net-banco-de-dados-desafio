e6d51eec6250f508f599e62700d2d98b5e8dae97
Cristiano <cristianofull@gmail.com>
2023-10-30 19:14:01 -0300

    Atualização arquivo trilha net banco de dados desafio

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,78 @@
+-- 1
+SELECT 
+	Nome, 
+	Ano 
+FROM Filmes
+
+--2
+SELECT 
+	Nome, 
+	Ano, 
+	Duracao 
+FROM Filmes
+ORDER BY Ano ASC
+
+--3
+SELECT 
+	Nome, 
+	Ano, 
+	Duracao 
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
+
+--4
+SELECT 
+	Nome, 
+	Ano, 
+	Duracao 
+FROM Filmes
+WHERE Ano = 1997
+
+--5
+SELECT 
+	Nome, 
+	Ano, 
+	Duracao 
+FROM Filmes
+WHERE Ano >= 2000
+
+--6
+SELECT 
+	Nome, 
+	Ano, 
+	Duracao 
+FROM Filmes 
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC
+
+--7
+SELECT Ano, 
+       COUNT(*) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
+--8
+SELECT * FROM Atores
+WHERE Genero = 'M'
+
+--9
+SELECT 
+	Id, 
+	PrimeiroNome, 
+	UltimoNome, 
+	Genero 
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+--10
+SELECT 
+	F.Nome, 
+	G.Genero 
+FROM Filmes F 
+INNER JOIN FilmesGenero FG ON F.Id = FG.IdFilme INNER JOIN Generos G ON G.Id = FG.IdGenero 
+
 --11
 SELECT 
 	F.Nome, 
